c3c076ef8a6d0966833b2f1c98d04403d887de04
Jason <jhwang836@gmail.com>
2025-07-14 10:50:03 -0400

    updated

--- pymol_pdb_analysis.py
+++ pymol_pdb_analysis.py
@@ -7,7 +7,8 @@ filely=open("aukb_wt_128_256_alignment_DFG.txt", "w")
 
 #DFG=142-144
 #G_Loop=8-13
-#desired=input("Desired pos: ")
+#aC=43-53
+
 
 def stripy(name, prefix, suffix):
     # Remove the prefix
@@ -25,23 +26,42 @@ with open("aukb_domain_wt_128_256.txt", "r") as f:
 
 # cmd.load("AukB_G_Loop.pdb")
 # cmd.color_deep("red", "AukB_G_Loop", 0)
-
-for i in range(len(curr_pdb)):
-    cmd.load("AukB_DFG.pdb")
-    cmd.color_deep("red", "AukB_DFG", 0)
-    paths=f"{curr_pdb[i]}"
-    cmd.load(paths)
-    cleaned_name = stripy(paths, "/Users/jasonhwang/Documents/Brown/Rubenstein/aukb_domain_wt_128_256/", ".pdb")
-    #cmd.color_deep("yellow", f'{cleaned_name}', 0)
+def annoatate():
     cmd.select("DFG", "resi 142-144")
     cmd.color_deep("Green", "DFG")
-    curr_data=cmd.align("polymer and name CA and (AukB_DFG)","polymer and name CA and (DFG)",quiet=0,object="aln_AukB_DFG_to_DFG",reset=1)
-#    curr_data=cmd.align("polymer and name CA and (AukB_G_Loop)","polymer and name CA and (AukB_G_Loop)",quiet=0,object="aln_AukB_G_Loop_to_G_Loop",reset=1)
-    data.append(f"{cleaned_name}|{curr_data} \n")
-    cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdb/{cleaned_name}_DFG", 0, 0, -1, ray=0)
-    cmd.delete("all")
+    cmd.select("G_Loop", "resi 8-13")
+    cmd.color_deep("Blue", "G_Loop")
+    cmd.select("aC_helix", "resi 43-53")
+    cmd.color_deep("Wheat", "aC_helix")
+def DFG():
+    for i in range(len(curr_pdb)):
+        cmd.load("AukB_DFG.pdb")
+        cmd.color_deep("red", "AukB_DFG", 0)
+        paths=f"{curr_pdb[i]}"
+        cmd.load(paths)
+        cleaned_name = stripy(paths, "/Users/jasonhwang/Documents/Brown/Rubenstein/aukb_domain_wt_128_256/", ".pdb")
+        #cmd.color_deep("yellow", f'{cleaned_name}', 0)
+        annoatate()
+        curr_data=cmd.align("polymer and name CA and (AukB_DFG)","polymer and name CA and (DFG)",quiet=0,object="aln_AukB_DFG_to_DFG",reset=1)
+        data.append(f"{cleaned_name}|{curr_data} \n")
+        cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdb/{cleaned_name}_DFG", 0, 0, -1, ray=0)
+        cmd.delete("all")
+def G_Loop():
+    for i in range(len(curr_pdb)):
+        cmd.load("AukB_G_Loop.pdb")
+        cmd.color_deep("red", "AukB_G_Loop", 0)
+        paths=f"{curr_pdb[i]}"
+        cmd.load(paths)
+        cleaned_name = stripy(paths, "/Users/jasonhwang/Documents/Brown/Rubenstein/aukb_domain_wt_128_256/", ".pdb")
+        #cmd.color_deep("yellow", f'{cleaned_name}', 0)
+        annoatate()
+        curr_data=cmd.align("polymer and name CA and (AukB_G_Loop)","polymer and name CA and (AukB_G_Loop)",quiet=0,object="aln_AukB_G_Loop_to_G_Loop",reset=1)
+        data.append(f"{cleaned_name}|{curr_data} \n")
+        cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdb/{cleaned_name}_DFG", 0, 0, -1, ray=0)
+        cmd.delete("all")
+
+
 
 print(data)
 for lines in data:
     filely.write(lines)
-
