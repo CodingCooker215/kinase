f8d55dd4413fad19dee6f3f6f4034693f56cd8e0
Jason <jhwang836@gmail.com>
2025-07-14 11:57:35 -0400

    modified_analysis_script

--- pymol_pdb_analysis.py
+++ pymol_pdb_analysis.py
@@ -1,6 +1,3 @@
-#need to identify DFG and GXGXXG loop in generated predicitons, then align based on sequence
-
-
 curr_pdb=[]
 data=[]
 filely=open("aukb_wt_128_256_alignment_DFG.txt", "w")
@@ -29,22 +26,45 @@ with open("aukb_domain_wt_128_256.txt", "r") as f:
 def annoatate():
     cmd.select("DFG", "resi 142-144")
     cmd.color_deep("Green", "DFG")
+    cmd.set("cartoon_transparency", 0, "DFG")
+
     cmd.select("G_Loop", "resi 8-13")
     cmd.color_deep("Blue", "G_Loop")
+    cmd.set("cartoon_transparency", 0, "G_Loop")
+
+
     cmd.select("aC_helix", "resi 43-53")
     cmd.color_deep("Wheat", "aC_helix")
+    cmd.set("cartoon_transparency", 0, "aC_helix")
+
+
+    cmd.select("F219", "resi 143")
+    cmd.show("sticks", "F219")
+    cmd.color_deep("pink", "F219")
+    cmd.set("cartoon_transparency", 0, "F219")
+
 def DFG():
     for i in range(len(curr_pdb)):
+        print(i)
         cmd.load("AukB_DFG.pdb")
         cmd.color_deep("red", "AukB_DFG", 0)
         paths=f"{curr_pdb[i]}"
         cmd.load(paths)
         cleaned_name = stripy(paths, "/Users/jasonhwang/Documents/Brown/Rubenstein/aukb_domain_wt_128_256/", ".pdb")
-        #cmd.color_deep("yellow", f'{cleaned_name}', 0)
+        cmd.color_deep("white", f'{cleaned_name}', 0)
+        cmd.set("cartoon_transparency", 0.75, f"{cleaned_name}")
         annoatate()
         curr_data=cmd.align("polymer and name CA and (AukB_DFG)","polymer and name CA and (DFG)",quiet=0,object="aln_AukB_DFG_to_DFG",reset=1)
         data.append(f"{cleaned_name}|{curr_data} \n")
-        cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdb/{cleaned_name}_DFG", 0, 0, -1, ray=0)
+        cmd.center()
+        cmd.set_view((
+      0.364821821,    0.235291421,    0.900856614,
+     0.666502714,    0.609603405,   -0.429134250,
+    -0.650137544,    0.756981492,    0.065574400,
+     0.000000000,    0.000000000, -187.763702393,
+     1.059869766,   -1.174396515,    1.122215271,
+  -6261.794921875, 6637.324707031,  -20.000000000 ))
+        cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdbtest/{cleaned_name}_DFG_test2", 0, 0, -1, ray=0)
         cmd.delete("all")
 def G_Loop():
     for i in range(len(curr_pdb)):
@@ -60,9 +80,8 @@ def G_Loop():
         cmd.png(f"/Users/jasonhwang/Documents/Brown/Rubenstein/pdb/{cleaned_name}_DFG", 0, 0, -1, ray=0)
         cmd.delete("all")
 
-
+DFG()
 
 print(data)
 for lines in data:
     filely.write(lines)
-    
